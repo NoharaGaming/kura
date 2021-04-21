10fde72bebd26c7211a1d07910d1848fb50b3b0c
NoharaGaming <80111092+NoharaGaming@users.noreply.github.com>
2021-04-21 20:40:21 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -3,7 +3,7 @@ class Dustbin{
         var options = {
             'restitution':0.8,
             friction:10.0,
-              isStaric:true,  
+              isStatic:true,  
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
